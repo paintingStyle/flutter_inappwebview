1a06ba08092318eb88eb5d0b5d2c86b80df09991
paintingStyle <sfdeveloper@163.com>
2023-03-16 10:37:45 +0800

    Update UserScript.swift

--- ios/Classes/Types/UserScript.swift
+++ ios/Classes/Types/UserScript.swift
@@ -4,14 +4,23 @@
 //
 //  Created by Lorenzo Pichilli on 16/02/21.
 //
-
 import Foundation
 import WebKit
 
 public class UserScript : WKUserScript {
     var groupName: String?
+
+    private var contentWorldWrapper: Any?
     @available(iOS 14.0, *)
-    lazy var contentWorld: WKContentWorld = WKContentWorld.page
+    var contentWorld: WKContentWorld {
+      get {
+        if let value = contentWorldWrapper as? WKContentWorld {
+          return value
+        }
+        return .page
+      }
+      set { contentWorldWrapper = newValue }
+    }
     
     public override init(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool) {
         super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly)
@@ -47,7 +56,7 @@ public class UserScript : WKUserScript {
                 groupName: map["groupName"] as? String,
                 source: map["source"] as! String,
                 injectionTime: WKUserScriptInjectionTime.init(rawValue: map["injectionTime"] as! Int) ?? .atDocumentStart,
-                forMainFrameOnly: map["iosForMainFrameOnly"] as! Bool,
+                forMainFrameOnly: map["forMainFrameOnly"] as! Bool,
                 in: contentWorld
             )
         }
@@ -55,7 +64,7 @@ public class UserScript : WKUserScript {
             groupName: map["groupName"] as? String,
             source: map["source"] as! String,
             injectionTime: WKUserScriptInjectionTime.init(rawValue: map["injectionTime"] as! Int) ?? .atDocumentStart,
-            forMainFrameOnly: map["iosForMainFrameOnly"] as! Bool
+            forMainFrameOnly: map["forMainFrameOnly"] as! Bool
         )
     }
 }
